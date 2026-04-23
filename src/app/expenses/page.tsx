34171d3054fce193183319809c6091c5126e69b6
Arley Cuadrado Sierra <arley.cuadradosierra@gmail.com>
2026-04-23 17:26:22 -0500

    expenses aside basic styles

--- src/app/expenses/page.tsx
+++ src/app/expenses/page.tsx
@@ -1,19 +1,47 @@
 "use client"
 
-import { useExpenses } from "@/context/ExpensesContext"
+
+import { useExpenses } from "@/context/ExpensesContext";
 
 export default function ExpensesPage() {
 
-    const { expenses } = useExpenses()
+    const { expenses } = useExpenses();
 
     console.log("Prubea contexto...", expenses)
 
     return (
         <>
-            <header className="pb-8">
-                <h2 className="mt-2 text-2xl font-semibold text-black">Gastos</h2>
-                <p>Consulta, filtra y administra tus gastos.</p>
-            </header>
+            <section>
+                <header className="pb-8">
+                    <h2 className="mt-2 text-2xl font-semibold text-black">Gastos</h2>
+                    <p>Consulta, filtra y administra tus gastos.</p>
+                </header>
+                <aside>
+                    <h2 className="pb-8">Gastos por categoría</h2>
+                    {expenses.map((expense) => (
+                        <div key={expense.id} className="flex flex-col gap-3 sm:flex-row sm:items-start sm:justify-between rounded border border-gray-200 bg-white mb-4 p-4">
+                            <div>
+                                <h3 className="font-medium text-black">{expense.description}</h3>
+                                <p className="mt-1 text-sm text-gray-600">
+                                    {expense.category} · {expense.date}
+                                </p>
+                            </div>
+                            <div className="flex flex-col">
+                                <p className="text-lg font-semibold text-black">{expense.amount}</p>
+                                <button>Delete</button>
+                            </div>
+                        </div>
+                    ))
+                    }
+                </aside>
+            </section>
         </>
     )
 }
+
+
+
+
+
+
+
